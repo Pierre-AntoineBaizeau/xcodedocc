ef4c643db80ff3b9f18c3e9f299b646bfffb0b11
BAIZEAU Pierre-Antoine <pabaizeau@roole.fr>
2024-06-04 11:26:03 +0200

    firsy

---
+++ xcodedocc/xcodedoccApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodedoccApp.swift
+//  xcodedocc
+//
+//  Created by BAIZEAU Pierreantoine on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct xcodedoccApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
